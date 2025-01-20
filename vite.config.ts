140d1881a4fd7a0217e9544cde32f66cc4619738
Kirill <kirill_baskakov12@mail.ru>
2025-01-20 19:47:34 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "cats"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
