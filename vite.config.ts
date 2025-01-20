77880799e6b53d2a7e6a2f9ce0c2a8891a0b179d
Kirill <kirill_baskakov12@mail.ru>
2025-01-20 19:48:53 +0300

    fix: vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/frontend-challenge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
